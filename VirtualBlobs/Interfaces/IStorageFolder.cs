b46e929b8b003379536be0420a6439a0f03ed65e
Michael <michel.koch@cliap.de>
2022-03-02 12:46:44 +0100

    init

---
+++ VirtualBlobs/Interfaces/IStorageFolder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VirtualBlobs.Interfaces
+{
+    public interface IStorageFolder
+    {
+        string GetPath();
+        string GetName();
+        long GetSize();
+        DateTime GetLastUpdated();
+        IStorageFolder GetParent();
+    }
+}
